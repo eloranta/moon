a43268b25fef6689d982901f7869c005d1ccce38
Hannu Eloranta <hannu.eloranta@gmail.com>
2019-05-26 22:29:11 +0300

    Revert "added line chart"
    This reverts commit 4316a73d58d9eb283486e1109f464d981f7dada7.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChartsModule } from 'ng2-charts';
-import { MyLineChartComponent } from './my-line-chart/my-line-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyLineChartComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

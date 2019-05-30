b07a81e2da63a3476bf0749c612f1372a4b65991
Hannu Eloranta <hannu.eloranta@gmail.com>
2019-05-30 18:36:59 +0300

    added random data

--- src/app/my-bar-chart/employee.component.ts
+++ src/app/my-bar-chart/employee.component.ts
@@ -1,27 +1,57 @@
-import {Component, OnChanges, SimpleChanges, Input} from '@angular/core';
+import {Component, OnChanges, SimpleChanges, Input, ViewChild} from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
  
 @Component({
   selector: 'app-emp',
   templateUrl: './employee.component.html'
 })
 export class EmployeeComponent implements OnChanges {
-  @Input() message: string;	
-  allMsgChangeLogs: string[] = [];
-  allEmployeeChangeLogs: string[] = [];
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective
+
+  @Input() locator: string;	
+  newLocator: string;
+  constructor(){
+  }
+  public barChartOptions = {
+    responsive: true
+  };
+  public barChartLabels = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'];
+  public barChartData = [
+    { data: [0], label: 'Account A' },
+    { data: [0], label: 'Account B' },
+  ]
   
   ngOnChanges(changes: SimpleChanges) {
 	  
 	for (let propName in changes) {  
-		let change = changes[propName];
 		
-		let curVal  = JSON.stringify(change.currentValue);
-		let prevVal = JSON.stringify(change.previousValue);
-		let changeLog = `${propName}: currentValue = ${curVal}, previousValue = ${prevVal}`;
-		
-		if (propName === 'message') {
-		   this.allMsgChangeLogs.push(changeLog);
-		} else if (propName === 'employee') {
-		   this.allEmployeeChangeLogs.push(changeLog);
+		if (propName === 'locator') {
+		    this.newLocator = changes[propName].currentValue.toLowerCase();
+		if (this.newLocator.length != 6) continue
+			if (this.newLocator[0] < 'a') continue
+			if (this.newLocator[0] > 'r') continue
+			if (this.newLocator[1] < 'a') continue
+			if (this.newLocator[1] > 'r') continue
+			if (this.newLocator[2] < '0') continue
+			if (this.newLocator[2] > '9') continue
+			if (this.newLocator[3] < '0') continue
+			if (this.newLocator[3] > '9') continue
+			if (this.newLocator[4] < 'a') continue
+			if (this.newLocator[4] > 'x') continue
+			if (this.newLocator[5] < 'a') continue
+			if (this.newLocator[5] > 'x') continue
+			this.locator = this.newLocator
+			
+			var hour:number
+			for (hour = 0; hour <= 24; hour++) {
+				this.barChartData[0].data[hour] = hour
+				this.barChartData[1].data[hour] = Math.floor(Math.random() * 6) + 1  
+			}
+			if (this.chart && this.chart.chart){
+			    this.chart.chart.update()
+				console.log(this.locator )
+			}
+    
 		}
     }
   }
--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -6,32 +6,13 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./my-bar-chart.component.css']
 })
 export class MyBarChartComponent{
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective
-  locator: string = 'kp11mk';
+  locatora: string = 'kp11mk';
 
   constructor(){
-  	var hour:number
-	for (hour = 0; hour <= 24; hour++) {
-		this.barChartData[0].data[hour] = hour
-		this.barChartData[1].data[hour] = hour + 1
-   }
-  }
-  public barChartOptions = {
-    responsive: true
-  };
-  public barChartLabels = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'];
-  public barChartData = [
-    { data: [0], label: 'Account A' },
-    { data: [0], label: 'Account B' },
-  ]
-  public onChartClick(e: any): void {
+
+    }
+   public onChartClick(e: any): void {
 	  console.log('hello')
-	  
-	// var hour:number
-	// for (hour = 0; hour <= 24; hour++) {
-		// this.barChartData[0].data[hour] = hour
-		// this.barChartData[1].data[hour] = hour + 2
-    // }
-	// this.chart.chart.update()
+
   }
 }

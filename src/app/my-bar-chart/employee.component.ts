eb80bd897e9083625c4f740e4e5f20ca82d6127b
Hannu Eloranta <hannu.eloranta@gmail.com>
2019-05-31 21:23:19 +0300

    feature: axis labels

--- src/app/my-bar-chart/employee.component.ts
+++ src/app/my-bar-chart/employee.component.ts
@@ -475,15 +475,25 @@ export class EmployeeComponent implements OnChanges {
     this.sun = new Sun
     this.moon = new Moon
   }
+ 
   barChartOptions = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    min: 0,
-                }
-            }]
-        },	  
-	  
+  scales: {
+    yAxes: [{
+	ticks: {
+      min: 0,
+      },
+      scaleLabel: {
+        display: true,
+        labelString: 'Elevation [degrees]'
+      }
+    }],
+    xAxes: [{
+      scaleLabel: {
+        display: true,
+        labelString: 'Time [UTC]'
+      }
+    }]
+  },
     responsive: true
   }
   public barChartLabels = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'];

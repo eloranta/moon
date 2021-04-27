a04315bdfca0786736aee7e4feb12d43c4815656
hannu.eloranta <oh2lhe@gmail.com>
2021-04-27 20:14:35 +0300

    change: tooltip content

--- src/app/line-chart/moon.component.ts
+++ src/app/line-chart/moon.component.ts
@@ -482,18 +482,22 @@ export class MoonComponent implements OnChanges {
     this.dayNumber = this.julianDayNumber(this.utcYear, this.utcMonth, this.utcDay, this.utcHour + this.utcMinutes / 60.0 + this.utcSeconds / 3600.0)
   }
  
-  barChartOptions = {
+  barChartOptions: any = {
 
     tooltips: {
+      displayColors: false,
       callbacks: {
+        title: function(tooltipItems, data) {
+           return 'UTC ' + tooltipItems[0].xLabel
+        },
+        afterTitle: function(tooltipItems, data) {
+           return 'Moon:'
+        },
         label: function(tooltipItem, data) {
-          var label = data.datasets[tooltipItem.datasetIndex].label || ''
-
-          if (label) {
-            label += ': '
-          }
-          label += Math.round(tooltipItem.yLabel * 10) / 10
-          return label
+          return 'Elevation ' + Math.round(tooltipItem.yLabel * 10) / 10
+        },
+        footer: function (tooltipItem, data) {
+          return 'Azimuth  ' + Math.round(data.datasets[0].azimuth[tooltipItem[0].index]) / 10;
         },
       }
     },  
@@ -520,7 +524,7 @@ export class MoonComponent implements OnChanges {
 
   public barChartLabels = ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00', '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21.30', '22:00', '22:30', '23:00', '23:30', '24:00']
   public barChartData = [
-    { data: [null], label: 'My locator' },
+    { data: [null], label: 'My locator', azimuth: [null] },
     { data: [null], label: 'DX locator' },
   ]
 
@@ -559,6 +563,7 @@ export class MoonComponent implements OnChanges {
       for (i = 0; i <= 48; i++) {
         var dayNumber = this.julianDayNumber(this.utcYear, this.utcMonth, this.utcDay, i/2.0)
         this.barChartData[0].data[i] = this.moon.elevation(dayNumber, this.myLongitude, this.myLatitude, this.sun, this.earth)
+        this.barChartData[0].azimuth[i] = this.moon.azimuth(dayNumber, this.myLongitude, this.myLatitude, this.sun, this.earth)
        }
       this.barChartData[0].label = this.locator
     }
